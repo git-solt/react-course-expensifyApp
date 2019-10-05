4c0a5903c074b9a0ea9a63ccac16f93fc06ba9cc
Torje <dev_sol.t@protonmail.com>
2019-10-05 22:20:44 +0200

    changing webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = (env) => {
       },
       plugins: [
         CSSExtract,
-        new webpack.DefinePluin({
+        new Webpack.DefinePluin({
           'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
           'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
           'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
